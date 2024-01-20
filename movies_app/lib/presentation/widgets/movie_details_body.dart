64c204d688b351f5c2be26795255153cc471e9e3
MohamedNasr12392 <midodaana912@gmail.com>
2024-01-20 18:42:35 +0200

    finish movie details page

--- movies_app/lib/presentation/widgets/movie_details_body.dart
+++ movies_app/lib/presentation/widgets/movie_details_body.dart
@@ -1,28 +1,146 @@
 import 'package:flutter/material.dart';
 import 'package:movies_app/models/movie/movie.dart';
 import 'package:movies_app/presentation/widgets/custom_movie_image.dart';
+import 'package:read_more_text/read_more_text.dart';
 
 class MovieDetailsBody extends StatelessWidget {
   const MovieDetailsBody({
-    super.key,
+    Key? key,
     required this.movie,
-  });
+  }) : super(key: key);
 
   final MovieModel movie;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          Hero(
-            tag: movie.show!.id.toString(),
-            child: CustomMovieImage(
-              imageUrl: movie.show!.image?.original.toString() ?? '',
+        backgroundColor: Colors.black,
+        body: ListView(children: [
+          Column(children: [
+            Stack(
+              children: [
+                Hero(
+                  tag: movie.show!.id.toString(),
+                  child: CustomMovieImage(
+                    imageUrl: movie.show!.image?.original.toString() ?? '',
+                  ),
+                ),
+                Positioned(
+                  top: 40,
+                  left: 20,
+                  child: IconButton(
+                    onPressed: () => Navigator.pop(context),
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          )
-        ],
-      ),
-    );
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.white,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            color: Colors.red,
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.play_arrow_outlined,
+                              color: Colors.black,
+                            ),
+                          ),
+                          const Text(
+                            'Play now',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      width: double.infinity * .5,
+                      color: Colors.blueGrey,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            color: Colors.red,
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.download,
+                              color: Colors.black,
+                            ),
+                          ),
+                          const Text(
+                            'Download',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Column(
+                children: [
+                  Text(
+                    movie.show?.name?.toUpperCase() ?? 'No Available Name',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    movie.show!.genres!.join(' • '),
+                    style: const TextStyle(
+                      color: Colors.white54,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Text(
+                movie.show!.summary!,
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ]),
+        ]));
   }
 }
